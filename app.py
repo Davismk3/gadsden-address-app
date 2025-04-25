2961937492c961293a51f6d751468f92f0e1b22f
Michael Davis <105256214+Davismk3@users.noreply.github.com>
2025-04-25 11:12:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import pandas as pd
 from metaphone import doublemetaphone
 from rapidfuzz import fuzz, process
+import os
 
 # Set up Flask
 app = Flask(__name__)
@@ -29,4 +30,5 @@ def index():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
