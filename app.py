b303e0869b6290bf53114c24e94389e29619511d
Michael Davis <105256214+Davismk3@users.noreply.github.com>
2025-04-25 11:05:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from rapidfuzz import fuzz, process
 app = Flask(__name__)
 
 # Load and preprocess addresses
-df = pd.read_csv("gadsden_data_custom_words.csv")
+df = pd.read_csv("roads.csv")
 df["phonetic"] = df["address"].apply(lambda x: doublemetaphone(str(x))[0])
 
 # Matching function
